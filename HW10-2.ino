6d18230e2c49b67bdacf9fa8aa5b8cf319f79512
sidsaik <77861140+sidsaik@users.noreply.github.com>
2021-10-23 17:55:02 -0400

    Add files via upload

---
+++ HW10-2.ino
@@ -0,0 +1,34 @@
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(5, OUTPUT);
+  pinMode(6, OUTPUT);
+  pinMode(7, OUTPUT);
+  pinMode(8, INPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+int input = digitalRead(8);
+
+  
+  if(input==HIGH){
+  digitalWrite(5, HIGH);
+  delay(1000);
+  digitalWrite(5, LOW);
+  delay(1000);
+  digitalWrite(6, HIGH);
+  delay(1000);
+  digitalWrite(6, LOW);
+  delay(1000);
+  digitalWrite(7, HIGH);
+  delay(1000);
+  digitalWrite(7, LOW);
+  delay(1000);
+  }
+  else{
+  digitalWrite(5, LOW);
+  digitalWrite(6, LOW);
+  digitalWrite(7, LOW);
+  }
+}
